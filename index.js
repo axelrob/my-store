f48c8115230968b57fa77a9a2e4ebb1659a607ab
Roberto Espinoza <axelrob@gmail.com>
2021-12-29 14:36:02 -0500

    Realizando rutas estaticas y dinamicas

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express=require('express');
+const faker = require('faker')
 const app=express();
 const port=12000;
 
@@ -10,17 +11,23 @@ app.get('/nueva-ruta', (req, res) =>{
   res.send('Mi nueva ruta');
 })
 
-app.get('/Products', (req, res)=>{
-  res.json([
-    {
-      name: 'Product 1',
-      price: 500
-    },
-    {
-      name: 'Product 2',
-      price: 1000
-    }
-  ])
+app.get('/products', (req, res)=>{
+  const products =[];
+  const {size}=req.query;
+  const limit = size || 10;
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.imageUrl()
+    });
+
+  }
+  res.json(products)
+})
+
+app.get('/products/filter', (req, res)=>{
+  res.send('Este es un filter')
 })
 
 app.get('/products/:id', (req, res)=>{
@@ -31,6 +38,28 @@ app.get('/products/:id', (req, res)=>{
     price: 2500
   });
 })
+
+
+
+/*Query params */
+
+app.get('/users', (req, res) =>{
+  const {limit, offset} = req.query;
+  if(limit && offset){
+    res.json({
+      limit,
+      offset
+    });
+  } else{
+    res.send('No hay parametros')
+  }
+})
+
+
+
+
+
+
 
 app.get('/products/:productsId/categories/:categoriesId', (req, res) =>{
   const {productsId,categoriesId} = req.params;
