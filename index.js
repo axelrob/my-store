dd15aacdc468e25d4c157a50d25a8e610397eabd
Roberto Espinoza <axelrob@gmail.com>
2022-01-12 21:29:23 -0500

    Se termina la etapa CRUD de las rutas con get, post, path, delete e inclusive codigos de estado HTTP en cada verbo

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express=require('express');
-const faker = require('faker')
+
 //Importar los modulos routes
 const routerApi=require('./routes');
 
@@ -7,6 +7,8 @@ const routerApi=require('./routes');
 
 const app=express();
 const port=12000;
+
+app.use(express.json());
 
 app.get('/', (req, res)=>{
   res.send('A darle');
@@ -22,33 +24,6 @@ routerApi(app);
 
 
 
-
-
-/*
-
-
-app.get('/products/:productsId/categories/:categoriesId', (req, res) =>{
-  const {productsId,categoriesId} = req.params;
-  res.json({
-    productsId,
-    categoriesId
-  });
-})
-/*Ejemplos con Marca, Modelo*/
-/*app.get('/marks/:marksId/models/:modelsId', (req, res)=>{
-  const {marksId,modelsId}=req.params;
-  res.json([
-    {
-      marksId,
-    },
-    {
-      modelsId,
-    }
-  ])
-})
-
-
-*/
 app.listen(port, () => {
   console.log('Mi port' + port);
 });
